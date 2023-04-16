1f80c0213457d4651013d91fd9a33c4716f910d6
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-04-16 21:29:02 +0300

    Summary

--- js/script.js
+++ js/script.js
@@ -1,18 +1,31 @@
 const btnAgain = document.querySelector('.again');
-const elLeft = document.querySelector('.left');
+const btnCheck = document.querySelector('.check');
+const numberInput = document.querySelector('.number-input');
 const guessNumber = Math.trunc(Math.random() * 20 + 1)
+const score = document.querySelector('.score');
+let count = 20
+console.log(guessNumber)
 
-function numberInput() { 
-    let userNumber = Number(elLeft.firstElementChild.value)
+function guessTheNumber() {    
+    let userNumber = Number(numberInput.value)
+    if (count > 1) {
     if (!userNumber) {
         alert('Enter number.')
     } else if (userNumber === guessNumber) {
-         document.querySelector('.question').textContent = 'You win!'
-    } else if (userNumber < guessNumber) { 
-        document.querySelector('.question').textContent = 'not enough'
-    } else if (userNumber > guessNumber) { 
+        document.querySelector('.question').textContent = 'You win!'               
+    } else if (userNumber < guessNumber) {        
+        count--
+        score.textContent = count
+        document.querySelector('.question').textContent = 'not enough'        
+    } else if (userNumber > guessNumber) {
+        count--
+        score.textContent = count
         document.querySelector('.question').textContent = 'too much'
     }
+    } else {        
+        score.textContent = 0
+        document.querySelector('.question').textContent = 'Game over!'
+        }    
 }
 
-elLeft.lastElementChild.addEventListener('click', numberInput)
+btnCheck.addEventListener('click', guessTheNumber)
