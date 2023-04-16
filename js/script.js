59026d4b2d3acea4ff246e1c149ee4d449e66581
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-04-16 18:58:56 +0300

    function numberInput()

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-'use strict';
+const btnAgain = document.querySelector('.again');
+const elLeft = document.querySelector('.left');
+const guessNumber = Math.trunc(Math.random() * 20 + 1)
+
+function numberInput() { 
+    let userNumber = Number(elLeft.firstElementChild.value)
+    if (!userNumber) {
+        alert('Enter number.')
+    } else if (userNumber === guessNumber) {
+         document.querySelector('.question').textContent = 'You win!'
+    } else if (userNumber < guessNumber) { 
+        document.querySelector('.question').textContent = 'not enough'
+    } else if (userNumber > guessNumber) { 
+        document.querySelector('.question').textContent = 'too much'
+    }
+}
+
+elLeft.lastElementChild.addEventListener('click', numberInput)
